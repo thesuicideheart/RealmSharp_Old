67d12e0dace8033dddd4c5c9092b3e516f22d574
thesuicideheart <tillolgamer@gmail.com>
2019-01-10 11:33:25 +0100

    Base classes

---
+++ Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealmSharp
+{
+    public class Character
+    {
+
+    }
+
+    internal enum ClassType
+    {
+
+    }
+
+}
